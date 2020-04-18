498d1e7cfaee679b3df7129f9e78d73f85852ea3
moonorange <monoma632@gmail.com>
2020-04-18 17:40:05 +0900

    fix: add other better solution to w2d1 and what I learnt

--- 30_day_challenge/middle_of_the_linked_list.py
+++ 30_day_challenge/middle_of_the_linked_list.py
@@ -1,9 +1,17 @@
+
+
+"""
+What I learnt
+Can find the middle of list by using slow and fast(2*slow) pointer
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+# brute force, dirty code
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
         node_size = 0
@@ -19,3 +27,18 @@ class Solution:
             count += 1
             head = head.next
         return ans
+
+class Solution_time_on:
+    def middleNode(self, head):
+        A = [head]
+        while A[-1].next:
+            A.append(A[-1].next)
+        return A[len(A) // 2
+
+class Solution_memory_on(object):
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
