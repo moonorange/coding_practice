77b05599be3bfab5fe4212423e441dfd2f5c1bc2
moonorange <monoma632@gmail.com>
2020-04-25 23:27:18 +0900

    feat: diameter_of_binary_tree

---
+++ 30_day_challenge/diameter_of_binary_tree.py
@@ -0,0 +1,24 @@
+#ノードの深さを求める関数
+def depth(p):
+    if not p: return 0
+    left, right = depth(p.left), depth(p.right)
+    return 1 + max(left, right)
+
+depth(root)
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.ans = 0
+
+        def depth(p):
+            if not p: return 0
+            left, right = depth(p.left), depth(p.right)
+            self.ans = max(self.ans, left+right)
+            return 1 + max(left, right)
+
+        depth(root)
+        return self.ans
