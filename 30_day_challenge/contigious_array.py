17d8a37b1ee7c65d6ae517a3da9d0758ea3a80a3
moonorange <monoma632@gmail.com>
2020-04-29 11:06:00 +0900

    feat: contigious array

--- 30_day_challenge/contigious_array.py
+++ 30_day_challenge/contigious_array.py
@@ -48,3 +48,21 @@ class Solution3:
             i += 1
 
         return max(global_max,current_max)
+
+class Solution_ex(object):
+    def findMaxLength(self, nums):
+        count = 0
+        max_length=0
+        table = {0: 0}
+        for index, num in enumerate(nums, 1):
+            if num == 0:
+                count -= 1
+            else:
+                count += 1
+
+            if count in table:
+                max_length = max(max_length, index - table[count])
+            else:
+                table[count] = index
+
+        return max_length
