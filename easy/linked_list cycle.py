a44efa94f531a448954489beb654307c0dd5c8be
moonorange <monoma632@gmail.com>
2021-02-24 08:45:18 +0900

    add: another O(1) memory solution to linked list loop

--- easy/linked_list cycle.py
+++ easy/linked_list cycle.py
@@ -1,10 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
+	def hasCycle(self, head: ListNode) -> bool:
 		nodes_set = set()
 		current = head
 		while (current):
-			if (current in nodes_set):
-				return True
+			if (current in nodes_set): return True
 			nodes_set.add(current)
 			current = current.next
+		return False
+
+	#O(1) memory
+	def hasCycle_(self, head: ListNode) -> bool:
+		walker = head
+		runner = head
+		while runner and runner.next:
+			walker = walker.next
+			runner = runner.next.next
+			if (walker == runner): return True
 		return False
