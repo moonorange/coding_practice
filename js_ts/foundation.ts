b1f87a9e65ca0a73f5d6dfea748e078ba147bb1c
Kiruthika Murugananthan <kiru@Kiruthikas-MacBook-Pro.local>
2021-05-03 14:33:08 +0900

    add: num to reversed num lists

--- js_ts/foundation.ts
+++ js_ts/foundation.ts
@@ -5,3 +5,10 @@ export const boolToWord = (bool: boolean): string => {
     return 'No';
   }
 };
+
+
+export const digitize = (n: number): number[] => {
+  const str_n = [...String(n)];
+  const num_list = str_n.map(x => Number(x));
+  return num_list.reverse();
+};
