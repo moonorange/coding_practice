32db65ffe6a3569f3c949fa2fffbeaa324b425f6
moonorange <monoma632@gmail.com>
2021-05-05 16:05:01 +0900

    add: better solution of seriese sum

--- js_ts/seriesSum.ts
+++ js_ts/seriesSum.ts
@@ -2,6 +2,7 @@
 
 // Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
 
+// My first attempt
 export function SeriesSum(n: number): string
 {
   if (n === 0) {
@@ -13,4 +14,15 @@ export function SeriesSum(n: number): string
   }
   const ans = 1 + sum_term
   return ans.toPrecision(3);
+}
+
+
+// Better solution in the code war
+export function SeriesSum2(n: number) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + (3 * i));
+  }
+
+  return sum.toFixed(2);
 }
