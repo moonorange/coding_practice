ee52f66ef8c2e55d6f1b0d61275636ab8c550aaa
moonorange <monoma632@gmail.com>
2021-05-09 21:24:23 +0900

    add: my third attempt of sum of pairs

--- js_ts/sum_of_pairs.ts
+++ js_ts/sum_of_pairs.ts
@@ -29,20 +29,43 @@ export function sumPairs2(ints: number[], s: number): [number, number] | void {
 	let ans: number[] = [];
 	for (let i = 0; i < ints.length; i++) {
 		for (let j = i + 1; j < ints.length; j++) {
-			if (i === j) { continue; }
 			if (ints[i] + ints[j] === s) {
 				if (!ans.length) {
 					ans = [i, j];
+					continue;
 				}
-				if (Math.max(i, j) < Math.max(...ans)) {
+				if (j < ans[1]) {
 					ans = [i, j];
 				}
 			}
 		}
 	}
-	// console.log(ans);
 	return ans.length ? [ints[ans[0]], ints[ans[1]]] : undefined;
 }
+
+
+// Third attempt though time complexity doesn't change
+export function sumPairs3(ints: number[], s: number): [number, number] | void {
+	let ans: number[] = [];
+	for (let i = 0; i < ints.length; i++) {
+		const findPair = (ele: number, index: number) => index != i && i < index && ele === s - ints[i];
+		if (!ans.length) {
+			let sec_idx = ints.findIndex(findPair);
+			if (0 <= sec_idx) {
+				ans = [i, sec_idx];
+				continue;
+			}
+		}
+		let sec_idx = ints.findIndex(findPair);
+		if (0 <= sec_idx && sec_idx < ans[1]) {
+			ans = [i, sec_idx];
+		}
+	}
+	const f_i = ans[0];
+	const s_i = ans[1];
+	return ans.length ? [ints[f_i], ints[s_i]] : undefined;
+}
+
 
 const l1: number[] = [1, 4, 8, 7, 3, 15],
 l2: number[] = [1, -2, 3, 0, -6, 1],
