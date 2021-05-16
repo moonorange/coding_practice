d800e3552428d586c9c98348bce0d5cd1e04879a
moonorange <monoma632@gmail.com>
2021-05-16 20:12:11 +0900

    add: buying a car solution

--- js_ts/saving_for_car.ts
+++ js_ts/saving_for_car.ts
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/554a44516729e4d80b000012/train/typescript
 
-// First failed attempt
+// First failed attempt cuz of misunderstanding of a problem
 export function nbMonths(startPriceOld: number, startPriceNew: number, savingperMonth: number, percentLossByMonth: number): number[] {
   if (startPriceNew <= startPriceOld) {
     return [0, Math.round(startPriceOld - startPriceNew)];
@@ -16,4 +16,19 @@ export function nbMonths(startPriceOld: number, startPriceNew: number, savingper
   }
 
   return [month, Math.round((startPriceOld + saving) - startPriceNew)]
+}
+
+
+export function nbMonths2(startPriceOld: number, startPriceNew: number, savingperMonth: number, percentLossByMonth: number): number[] {
+  let saving = 0;
+  let month = 0;
+  while (startPriceOld + saving < startPriceNew) {
+    startPriceOld = startPriceOld - startPriceOld * (percentLossByMonth / 100)
+    startPriceNew = startPriceNew - startPriceNew * (percentLossByMonth / 100)
+    saving += savingperMonth;
+    if (month % 2 == 0) percentLossByMonth += 0.5;
+    month++;
+  }
+
+  return [month, Math.round((startPriceOld + saving) - startPriceNew)];
 }
